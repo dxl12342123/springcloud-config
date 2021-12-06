d1f1d442be2825b3da4c6c49085b5abe4ba2f56d
dxl <20180210420419@ncjti.edu.cn>
2021-12-06 11:08:20 +0800

    123

---
+++ com/dxl/jas/domain/Student.java
@@ -0,0 +1,50 @@
+package com.dxl.jas.domain;
+
+public class Student {
+    private Integer id;
+    private String age;
+    private String name;
+    private String cls;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", age='" + age + '\'' +
+                ", name='" + name + '\'' +
+                ", cls='" + cls + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCls() {
+        return cls;
+    }
+
+    public void setCls(String cls) {
+        this.cls = cls;
+    }
+}
